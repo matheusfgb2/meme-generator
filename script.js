adcc39d6cb7ed696cb1a37a3befcfd096bb2fd84
Matheus Boechat <matheusfgb@gmail.com>
2023-02-13 20:55:54 -0300

    requisitos 2, 3, 4 e 5

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const insertMeme = document.getElementById('meme-insert');
+const memeImg = document.getElementById('meme-image');
 
 textInput.addEventListener('keyup', (event) => {
   memeText.innerHTML = event.target.value;
+});
+
+insertMeme.addEventListener('change', (event) => {
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+
 });
